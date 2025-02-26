1bbc57211d6e7a1881b759f531e54b311f657a6b
Kmuzaki <keihanmuzaki0480@gmail.com>
2025-02-26 21:38:50 +0800

    add code to randomly display Java fact

--- src/MyFirstProgram.java
+++ src/MyFirstProgram.java
@@ -5,18 +5,18 @@ public class MyFirstProgram {
         // Initialise necessary variables for the program
         String username;
         String javaVersion = System.getProperty("java.version"); // Retrieve Java version
-        // boolean doneListeningFacts = false;
+        boolean doneListeningFacts = false;
         
         // Storing multiple facts about Java within an array
         String[] javaFacts = {"Java in a nutshell was created by a group of people who despise the C programming language.", 
         "Java was initially named 'Oak' , however it was later renamed Java due to trademark issues. Which coincidentally",
         "Java was built with the 'Write Once, Run Anywhere' (WORA) Ideology, meaning that a compiled Java code could run on any device with Java Virtual Machine (JVM) installed. Even modern cars could run Java! Incredible, don't you think?",
-        "Java was created by James Gosling, Mike Sheridan, Patrick Naughton, and others at Sun Microsystems in 1995",
+        "Java was created by James Gosling, Mike Sheridan, Patrick Naughton, and others at Sun Microsystems in 1995.",
         "The name Java was inspired by the developers' love for Java Coffee. Hence the popular coffee cup logo associated with Java you see today.",
         "The revolutionary 'Write Once, Run Anywhere' (WORA) concept eliminated the need for developers to create separate versions of their software for different platforms.",
         "Java is a fully object-oriented language, which means that it is built around the concept of 'objects', which are entities that combine data and behavior.",
         "Java's object-oriented approach not only simplifies the development of complex systems, but also makes code more maintainable and scalable.",
-        "Java was heavily influenced by C++ but removed complex features like pointers and operator overloading",
+        "Java was heavily influenced by C++ but removed complex features like pointers and operator overloading.",
         "Although Java was released in 1995. The actual first Java Development Kit or JDK 1.0 was released in 1996.",
         "Java has an automatic memory management feature called 'Garbage Collection'. It Java programs run more efficiently by remove unused objects, freeing up memory in the background.",
         };
@@ -27,6 +27,8 @@ public class MyFirstProgram {
          - GeeksforGeeks, Java Memory Management, https://www.geeksforgeeks.org/java-memory-management/
          - GeeksforGeeks, Garbage Collection in Java, https://www.geeksforgeeks.org/garbage-collection-java/
         */
+
+        int arrayCursor = 0;
 
         // Initialise scanner for user input
         Scanner scanner = new Scanner(System.in);
@@ -47,11 +49,25 @@ public class MyFirstProgram {
             System.out.println("Freeman!? What are the odds that you have the exact same name as my friend's last name?");
         } else {
             System.out.println("Ah I see! Well, it's nice to meet you, " + username + "!");
+            System.out.println("");
         }
 
+        // Print a Java fact
+        // do {
+            arrayCursor = (int) (Math.random() * 11);
+            System.out.println("Did you know? " + javaFacts[arrayCursor]);
+        // }
+        // while (doneListeningFacts == false);
+          
+
+        // Prints out current Java version
         System.out.println("It seems like you currently have Java version " + javaVersion + " installed.");
 
         // Close the scanner
         scanner.close();
     }
 }
+
+/*
+ 
+ */
