3c74a59d7eb57c9db0f4255fcba717c7ebba3df7
Kmuzaki <keihanmuzaki0480@gmail.com>
2025-02-26 21:22:17 +0800

    adding conditions for specific names

--- src/MyFirstProgram.java
+++ src/MyFirstProgram.java
@@ -4,16 +4,31 @@ public class MyFirstProgram {
     public static void main(String[] args){
         // Initialise necessary variables for the program
         String username;
-        String javaVersion = System.getProperty("java.version");
+        String javaVersion = System.getProperty("java.version"); // Retrieve Java version
         // boolean doneListeningFacts = false;
         
         // Storing multiple facts about Java within an array
         String[] javaFacts = {"Java in a nutshell was created by a group of people who despise the C programming language.", 
         "Java was initially named 'Oak' , however it was later renamed Java due to trademark issues. Which coincidentally",
         "Java was built with the 'Write Once, Run Anywhere' (WORA) Ideology, meaning that a compiled Java code could run on any device with Java Virtual Machine (JVM) installed. Even modern cars could run Java! Incredible, don't you think?",
-        ""};
+        "Java was created by James Gosling, Mike Sheridan, Patrick Naughton, and others at Sun Microsystems in 1995",
+        "The name Java was inspired by the developers' love for Java Coffee. Hence the popular coffee cup logo associated with Java you see today.",
+        "The revolutionary 'Write Once, Run Anywhere' (WORA) concept eliminated the need for developers to create separate versions of their software for different platforms.",
+        "Java is a fully object-oriented language, which means that it is built around the concept of 'objects', which are entities that combine data and behavior.",
+        "Java's object-oriented approach not only simplifies the development of complex systems, but also makes code more maintainable and scalable.",
+        "Java was heavily influenced by C++ but removed complex features like pointers and operator overloading",
+        "Although Java was released in 1995. The actual first Java Development Kit or JDK 1.0 was released in 1996.",
+        "Java has an automatic memory management feature called 'Garbage Collection'. It Java programs run more efficiently by remove unused objects, freeing up memory in the background.",
+        };
 
-        // Initiate scanner for user input
+        /* References for Java Facts 
+         - "Java: History, Features, and Ecosystem" module from UC Elearn
+         - ChatGPT, https://chatgpt.com/share/67bf130b-8844-800c-acd5-7ff8101fcf02
+         - GeeksforGeeks, Java Memory Management, https://www.geeksforgeeks.org/java-memory-management/
+         - GeeksforGeeks, Garbage Collection in Java, https://www.geeksforgeeks.org/garbage-collection-java/
+        */
+
+        // Initialise scanner for user input
         Scanner scanner = new Scanner(System.in);
 
         // Print welcome message
@@ -25,7 +40,15 @@ public class MyFirstProgram {
         username = scanner.next();
 
         // Prints the message
-        System.out.println("Ah I see! Well, it's nice to meet you, " + username + "!");
+        if (username == "Gordon") {
+            System.out.println("Oh? Well that makes it easier for me to remember, actually. Since one of my colleagues is also named Gordon.");
+            System.out.println("Hahah. What a matter of coincidence!");
+        } else if (username == "Freeman") {
+            System.out.println("Freeman!? What are the odds that you have the exact same name as my friend's last name?");
+        } else {
+            System.out.println("Ah I see! Well, it's nice to meet you, " + username + "!");
+        }
+
         System.out.println("It seems like you currently have Java version " + javaVersion + " installed.");
 
         // Close the scanner
