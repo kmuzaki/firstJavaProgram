1c7c68b8b929be1c9a66eff3a7c19a0a2d8c85eb
Kmuzaki <keihanmuzaki0480@gmail.com>
2025-02-26 09:25:49 +0800

    init

---
+++ src/MyFirstProgram.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class MyFirstProgram {
+    public static void main(String[] args){
+        // Print welcome message
+        System.out.println("Hello IT enthusiast! What's your name?");
+
+        // Asks the user for name as input
+        
+    }
+}
